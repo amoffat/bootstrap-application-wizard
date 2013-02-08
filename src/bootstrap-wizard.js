efa9baf68e2385252a4631820eecbe3cb6b3662b
Andrew Moffat <andrew.robert.moffat@gmail.com>
2013-02-07 22:01:21 -0600

    adding license file and updating copyright date to 2013, closes #2

--- src/bootstrap-wizard.js
+++ src/bootstrap-wizard.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Panopta, Andrew Moffat
+ * Copyright (C) 2013 Panopta, Andrew Moffat
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
